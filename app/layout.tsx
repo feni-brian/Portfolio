b149a3b22995eeac0f8a88c88c95c72a8ade4072
Feni Brian <56452538+feni-brian@users.noreply.github.com>
2023-11-13 10:50:00 +0300

    Added code for light and dark browser icons.

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,20 +12,27 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Feni | Portfolio ",
-	description:
-		"Feni's is a full-stack developer with only a year's experience.",
+	description: "Feni is a full-stack developer with only a year's experience.",
+	icons: {
+		icon: [
+			{
+				media: "(prefers-color-scheme: dark)",
+				url: "/favicon-dark.png",
+				href: "/favicon-dark.png",
+			},
+			{
+				media: "(prefers-color-scheme: light)",
+				url: "/favicon.png",
+				href: "/favicon.png",
+			},
+		],
+	},
 };
 
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en" className="!scroll-smooth">
-			<body
-				className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
-			>
+			<body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
 				<div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]" />
 				<div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]" />
 				<ThemeContextProvider>
